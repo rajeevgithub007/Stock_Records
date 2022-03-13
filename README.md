a5febe5177bec414abf3ebdac3c5b489c2530e67
rajeevgithub007 <rajeevh07@gmail.com>
2022-03-13 11:15:19 +0000

    Modified stock_filter and added .gitignore, .knownErrors, .pylintrc, Notes, TSVFile.tsv, requirements.txt, setup.cfg

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 # {{"msgType_":12,"length_":25,"seqNo_":1779},"bookEntry_":{"securityId_":2,"side_":BUY,"quantity_":402,"price_":121550000,"orderId_":532424}}
 # 0 {{"msgType_":8,"length_":36,"seqNo_":281},"security_":{"securityId_":4491,"umtf_":"BVCl","isin_":"IL0010849045","currency_":"GBX","mic_":"XLON","tickTableId_":32,"flags_":"{"b_":{"closingEnabled_":1,"testStock_":0,"illiquid":0,"live_":1,"aodEnabled_":1},"v_":25}}}
 # with common key as security_id
-# Export th resultant list of dictionary records to a csv file 
+# Export the resultant list of dictionary records to a tsv file 
 
 ####################################
 
--- stock_filter.py
+++ stock_filter.py
@@ -77,179 +77,205 @@ def get_file_records_12(filename):
 
 def get_filtered_records(filename):
     """Get the resultant list of dictionaries in expected format."""
-    filtered_lst, res = [], []
-    lst_dict_8 = get_file_records_12(filename)
-    lst_dict_12 = get_file_records_8(filename)
+    try:
+        filtered_lst = []
+        lst_dict_8 = get_file_records_12(filename)
+        lst_dict_12 = get_file_records_8(filename)
 
-    for item in lst_dict_12:
-        filtered_lst.extend([dict(zip(['ISIN', 'Currency', 'side_', 'quantity_', 'price_', 'price_per_unit'],
-                                      [item['isin_'], item['currency_'], x['side_'], x['quantity_'],
-                                       x['price_'], x['price_']/x['quantity_']]))
-                             for x in lst_dict_8 if x['securityId_'] == item['securityId_']])
-    result = get_resultant_lst(filtered_lst)
-
-    return result
-
+        for item in lst_dict_12:
+            filtered_lst.extend([dict(zip(['ISIN', 'Currency', 'side_', 'quantity_', 'price_', 'price_per_unit'],
+                                          [item['isin_'], item['currency_'], x['side_'], x['quantity_'],
+                                           x['price_'], x['price_'] / x['quantity_']]))
+                                 for x in lst_dict_8 if x['securityId_'] == item['securityId_']])
+        result = get_resultant_lst(filtered_lst)
+        return result
+    except Exception as ex:
+        output_json = dict(zip(['Message', 'Payload'],
+                               [f'Encountered exception in reading contents of files : {ex} ',
+                                None]))
+        return output_json
 
 
 def get_resultant_lst(lst):
-    result_count = total_buy_sell_count(lst)
-    result_max_min = max_min_buy_sell_price(lst)
-    result_price=  price_(lst)
-    result_quantity= total_buy_sell_quantity(lst)
+    """Get the resultant aggregation of processes stock records."""
+    try:
+        result_count = get_total_buy_sell_count(lst)
+        result_max_min = get_max_min_buy_sell_price(lst)
+        result_price = get_total_buy_sale_price(lst)
+        result_quantity = get_total_buy_sell_quantity(lst)
 
-    comp_buy_count =[{'ISINCurrencyside_':f"{d['ISIN']}|{d['Currency']}|{d['side_']}",
-                      'Total Buy Count': d['Total Buy Count']} for d in result_count if d['side_'] == 'BUY']
-    comp_sell_count =[{'ISINCurrencyside_':f"{d['ISIN']}|{d['Currency']}|{d['side_']}",
-                       'Total Sell Count': d['Total Sell Count']} for d in result_count if d['side_'] == 'SELL']
+        comp_buy_count = [{'ISINCurrencyside_': f"{d['ISIN']}|{d['Currency']}|{d['side_']}",
+                           'Total Buy Count': d['Total Buy Count']} for d in result_count if d['side_'] == 'BUY']
+        comp_sell_count = [{'ISINCurrencyside_': f"{d['ISIN']}|{d['Currency']}|{d['side_']}",
+                            'Total Sell Count': d['Total Sell Count']} for d in result_count if d['side_'] == 'SELL']
 
-    comp_buy_quantity =[{'ISINCurrencyside_':f"{d['ISIN']}|{d['Currency']}|{d['side_']}",
-                         'Total Buy Quantity': d['Total Buy Quantity']} for d in result_quantity if d['side_'] == 'BUY']
-    comp_sell_quantity =[{'ISINCurrencyside_':f"{d['ISIN']}|{d['Currency']}|{d['side_']}",
-                          'Total Sell Quantity': d['Total Sell Quantity']} for d in result_quantity if d['side_'] == 'SELL']
-    comp_max_buy_price =[{'ISINCurrencyside_':f"{d['ISIN']}|{d['Currency']}|{d['side_']}",
-                              'Max Buy Price': d['Max Buy Price'] } for d in result_max_min if d['side_'] == 'BUY']
-    comp_min_sell_price =[{'ISINCurrencyside_':f"{d['ISIN']}|{d['Currency']}|{d['side_']}",
-                               'Min Sell Price': d['Min Sell Price'] } for d in result_max_min if d['side_'] == 'SELL']
-    comp_buy_price =[{'ISINCurrencyside_':f"{d['ISIN']}|{d['Currency']}|{d['side_']}",
-                      'Total Buy Price': d['Total Buy Price']} for d in result_price if d['side_'] == 'BUY']
-    comp_sell_price = [{'ISINCurrencyside_':f"{d['ISIN']}|{d['Currency']}|{d['side_']}",
-                        'Total Sell Price': d['Total Sell Price']} for d in result_price if d['side_'] == 'SELL']
+        comp_buy_quantity = [{'ISINCurrencyside_': f"{d['ISIN']}|{d['Currency']}|{d['side_']}",
+                              'Total Buy Quantity': d['Total Buy Quantity']}
+                             for d in result_quantity if d['side_'] == 'BUY']
 
-    d = defaultdict(dict)
+        comp_sell_quantity = [{'ISINCurrencyside_': f"{d['ISIN']}|{d['Currency']}|{d['side_']}",
+                               'Total Sell Quantity': d['Total Sell Quantity']} for d in result_quantity
+                              if d['side_'] == 'SELL']
 
-    for item in comp_buy_count + comp_sell_count + comp_buy_quantity + comp_sell_quantity + comp_buy_price + \
-                comp_sell_price + comp_max_buy_price + comp_min_sell_price:
-        d[item['ISINCurrencyside_']].update(item)
+        comp_max_buy_price = [{'ISINCurrencyside_': f"{d['ISIN']}|{d['Currency']}|{d['side_']}",
+                               'Max Buy Price': d['Max Buy Price']} for d in result_max_min if d['side_'] == 'BUY']
 
-    res_lst = list(d.values())
+        comp_min_sell_price = [{'ISINCurrencyside_': f"{d['ISIN']}|{d['Currency']}|{d['side_']}",
+                                'Min Sell Price': d['Min Sell Price']} for d in result_max_min if d['side_'] == 'SELL']
 
-    for d in res_lst:
-        if 'BUY' in d['ISINCurrencyside_']:
-            d['Weighted Average Buy Price'] = ((d['Total Buy Price'])/d['Total Buy Quantity'])
-        else:
-            d['Weighted Average Sell Price'] = ((d['Total Sell Price'])/d['Total Sell Quantity'])
-    for d in res_lst:
-        if 'BUY' in d['ISINCurrencyside_']:
-            del d['Total Buy Price']
-        if 'SELL' in d['ISINCurrencyside_']:
-            del d['Total Sell Price']
+        comp_buy_price = [{'ISINCurrencyside_': f"{d['ISIN']}|{d['Currency']}|{d['side_']}",
+                           'Total Buy Price': d['Total Buy Price']} for d in result_price if d['side_'] == 'BUY']
+        comp_sell_price = [{'ISINCurrencyside_': f"{d['ISIN']}|{d['Currency']}|{d['side_']}",
+                            'Total Sell Price': d['Total Sell Price']} for d in result_price if d['side_'] == 'SELL']
 
-    for d in res_lst:
-        key_list = ['ISIN', 'Currency', 'side_']
-        d['ISINCurrencyside_'] = d['ISINCurrencyside_'].split('|')
-        d[key_list[0]] = d['ISINCurrencyside_'][0]
-        d[key_list[1]] = d['ISINCurrencyside_'][1]
-        # d[key_list[2]] = d['ISINCurrencyside_'][2]
-        del d['ISINCurrencyside_']
-    return res_lst
+        d = defaultdict(dict)
+        for item in comp_buy_count + comp_sell_count + comp_buy_quantity + comp_sell_quantity + comp_buy_price + \
+                    comp_sell_price + comp_max_buy_price + comp_min_sell_price:
+            d[item['ISINCurrencyside_']].update(item)
+        res_lst = list(d.values())
+        for d in res_lst:
+            if 'BUY' in d['ISINCurrencyside_']:
+                d['Weighted Average Buy Price'] = ((d['Total Buy Price']) / d['Total Buy Quantity'])
+            else:
+                d['Weighted Average Sell Price'] = ((d['Total Sell Price']) / d['Total Sell Quantity'])
+        for d in res_lst:
+            if 'BUY' in d['ISINCurrencyside_']:
+                del d['Total Buy Price']
+            if 'SELL' in d['ISINCurrencyside_']:
+                del d['Total Sell Price']
 
-def total_buy_sell_count(lst):
-
-    grouper = itemgetter('ISIN', 'Currency', 'side_')
-
-    # print('grouper:', grouper)
-    result_count,  final_lst = [], []
-    for key, grp in groupby(sorted(lst, key=grouper), grouper):
-        temp_dict = dict(zip(["ISIN", "Currency", "side_"], key))
-
-        if temp_dict['side_'] == 'BUY':
-            temp_list = [item["price_"] for item in grp]
-            temp_dict["Total Buy Count"] = len(temp_list)
-
-        else:
-            temp_list = [item["price_"] for item in grp]
-            temp_dict["Total Sell Count"] = len(temp_list)
+        for d in res_lst:
+            key_list = ['ISIN', 'Currency', 'side_']
+            d['ISINCurrencyside_'] = d['ISINCurrencyside_'].split('|')
+            d[key_list[0]] = d['ISINCurrencyside_'][0]
+            d[key_list[1]] = d['ISINCurrencyside_'][1]
+            # d[key_list[2]] = d['ISINCurrencyside_'][2]
+            del d['ISINCurrencyside_']
+        return res_lst
+    except Exception as ex:
+        output_json = dict(zip(['Message', 'Payload'],
+                               [f'Encountered exception in reading contents of files : {ex} ', None]))
+        return output_json
 
 
-        result_count.append(temp_dict)
-        # result = result1 + result2
-    return result_count
+def get_total_buy_sell_count(lst):
+    """Get the total buy count and total sale count for selected stock records."""
+    try:
+        grouper = itemgetter('ISIN', 'Currency', 'side_')
+        result_count = []
+        for key, grp in groupby(sorted(lst, key=grouper), grouper):
+            temp_dict = dict(zip(["ISIN", "Currency", "side_"], key))
+
+            if temp_dict['side_'] == 'BUY':
+                temp_list = [item["price_"] for item in grp]
+                temp_dict["Total Buy Count"] = len(temp_list)
+
+            else:
+                temp_list = [item["price_"] for item in grp]
+                temp_dict["Total Sell Count"] = len(temp_list)
+
+            result_count.append(temp_dict)
+            # result = result1 + result2
+        return result_count
+    except Exception as ex:
+        output_json = dict(zip(['Message', 'Payload'],
+                               [f'Encountered exception in reading contents of files : {ex} ',
+                                None]))
+        return output_json
 
 
-def total_buy_sell_quantity(lst):
+def get_total_buy_sell_quantity(lst):
+    """Get the total buy quantity and total sale quantity for selected stock records."""
+    try:
+        grouper = itemgetter('ISIN', 'Currency', 'side_')
 
-    grouper = itemgetter('ISIN', 'Currency', 'side_')
+        result = []
+        for key, grp in groupby(sorted(lst, key=grouper), grouper):
+            temp_dict = dict(zip(['ISIN', 'Currency', 'side_'], key))
 
-    result = []
-    for key, grp in groupby(sorted(lst, key=grouper), grouper):
-        temp_dict = dict(zip(['ISIN', 'Currency', 'side_'], key))
+            if temp_dict['side_'] == 'BUY':
+                temp_dict["Total Buy Quantity"] = sum(item["quantity_"] for item in grp)
+            else:
+                # temp_lst = [item["quantity_"] for item in grp]
+                temp_dict["Total Sell Quantity"] = sum(item["quantity_"] for item in grp)
 
-
-        if temp_dict['side_'] == 'BUY':
-            temp_dict["Total Buy Quantity"] = sum(item["quantity_"] for item in grp)
-
-        else:
-            # temp_lst = [item["quantity_"] for item in grp]
-            temp_dict["Total Sell Quantity"] = sum(item["quantity_"] for item in grp)
-
-        result.append(temp_dict)
-    return result
-
-
-def max_min_buy_sell_price(lst):
-    grouper = itemgetter('ISIN', 'Currency', 'side_')
-    # print('grouper:', grouper)
-    result = []
-    for key, grp in groupby(sorted(lst, key=grouper), grouper):
-        temp_dict = dict(zip(['ISIN', 'Currency', 'side_'], key))
-
-        if temp_dict['side_'] == 'BUY':
-            temp_list = [item["price_per_unit"] for item in grp]
-
-            temp_dict["Max Buy Price"] = max(temp_list)
-            # temp_dict["Min_Buy_Price"] = min(temp_list)
-
-        else:
-            temp_list = [item["price_per_unit"] for item in grp]
-
-            # temp_dict["Max_Sell_Price"] = max(temp_list)
-            temp_dict["Min Sell Price"] = min(temp_list)
-
-        result.append(temp_dict)
-    return result
-
-def  price_(lst):
-    grouper = itemgetter('ISIN', 'Currency', 'side_')
-    # print('grouper:', grouper)
-    result = []
-    for key, grp in groupby(sorted(lst, key=grouper), grouper):
-        temp_dict = dict(zip(['ISIN', 'Currency', 'side_'], key))
-
-        if temp_dict['side_'] == 'BUY':
-            temp_list = [item["price_"] for item in grp]
-            temp_dict["Total Buy Price"]= sum(temp_list)
+            result.append(temp_dict)
+        return result
+    except Exception as ex:
+        output_json = dict(zip(['Message', 'Payload'],
+                               [f'Encountered exception in reading contents of files : {ex} ', None]))
+        return output_json
 
 
-        else:
-            temp_list = [item["price_"] for item in grp]
-            temp_dict["Total Sell Price"]= sum(temp_list)
+def get_max_min_buy_sell_price(lst):
+    """Get the Maximum buy price and Minimum sale price for selected stock records."""
+    try:
+        grouper = itemgetter('ISIN', 'Currency', 'side_')
+        result = []
+        for key, grp in groupby(sorted(lst, key=grouper), grouper):
+            temp_dict = dict(zip(['ISIN', 'Currency', 'side_'], key))
+            if temp_dict['side_'] == 'BUY':
+                temp_list = [item["price_per_unit"] for item in grp]
+                temp_dict["Max Buy Price"] = max(temp_list)
+                # temp_dict["Min_Buy_Price"] = min(temp_list)
+            else:
+                temp_list = [item["price_per_unit"] for item in grp]
+                # temp_dict["Max_Sell_Price"] = max(temp_list)
+                temp_dict["Min Sell Price"] = min(temp_list)
+            result.append(temp_dict)
+        return result
+    except Exception as ex:
+        output_json = dict(zip(['Message', 'Payload'],
+                               [f'Encountered exception in reading contents of files : {ex} ', None]))
+        return output_json
 
 
-        result.append(temp_dict)
-    return result
+def get_total_buy_sale_price(lst):
+    """Get the total buy price and total sale price for selected stock records."""
+    try:
+        grouper = itemgetter('ISIN', 'Currency', 'side_')
+        result = []
+        for key, grp in groupby(sorted(lst, key=grouper), grouper):
+            temp_dict = dict(zip(['ISIN', 'Currency', 'side_'], key))
+
+            if temp_dict['side_'] == 'BUY':
+                temp_list = [item["price_"] for item in grp]
+                temp_dict["Total Buy Price"] = sum(temp_list)
+
+            else:
+                temp_list = [item["price_"] for item in grp]
+                temp_dict["Total Sell Price"] = sum(temp_list)
+
+            result.append(temp_dict)
+        return result
+    except Exception as ex:
+        output_json = dict(zip(['Message', 'Payload'],
+                               [f'Encountered exception in reading contents of files : {ex} ',
+                                None]))
+        return output_json
 
 
-
-def export_to_tsv(filename):
+def export_to_tsv(filename, tsv_file):
     """ Export the resultant list of dictionaries in tabular format."""
-    lst = get_filtered_records(filename)
-    keys = ['ISIN', 'Currency', 'Total Buy Count', 'Total Sell Count', 'Total Buy Quantity', 'Total Sell Quantity',
-            'Weighted Average Buy Price', 'Weighted Average Sell Price', 'Max Buy Price', 'Min Sell Price']
+    try:
+        dir_name = os.path.dirname(os.path.abspath(__file__))
+        stock_filename = os.path.join(dir_name, filename)
+        lst = get_filtered_records(stock_filename)
+        keys = ['ISIN', 'Currency', 'Total Buy Count', 'Total Sell Count', 'Total Buy Quantity', 'Total Sell Quantity',
+                'Weighted Average Buy Price', 'Weighted Average Sell Price', 'Max Buy Price', 'Min Sell Price']
 
-    # ISIN | Currency | Total Buy Count | Total Sell Count | Total Buy Quantity | Total Sell Quantity | Weighted Average Buy Price | Weighted Average Sell Price | Max Buy Price | Min Sell Price
-    dir_name = os.path.dirname(os.path.abspath(__file__))
-    csv_filename = os.path.join(dir_name, 'TSVFile.tsv')
+        csv_filename = os.path.join(dir_name, tsv_file)
 
-    with open(csv_filename, 'w') as output_file:
-        dict_writer = csv.DictWriter(output_file, keys, dialect='excel-tab')
-        dict_writer.writeheader()
-        dict_writer.writerows(lst)
+        with open(csv_filename, 'w') as output_file:
+            dict_writer = csv.DictWriter(output_file, keys, dialect='excel-tab')
+            dict_writer.writeheader()
+            dict_writer.writerows(lst)
+    except Exception as ex:
+        output_json = dict(zip(['Message', 'Payload'],
+                               [f'Encountered exception in reading contents of files : {ex} ',
+                                None]))
+        return output_json
 
 
 if __name__ == '__main__':
-    # print('filtered_records: ', get_filter_records(r"C:/Users/Raj/stock_records"))
-    filtered_records = get_filtered_records(r"C:/Users/Raj/stock_records")
-    print(filtered_records)
-    # print('final_list', get_resultant_lst(filtered_records))
-    print(export_to_tsv(r"C:/Users/Raj/stock_records"))
+    export_to_tsv('stock_records', 'TSVFile.tsv')
